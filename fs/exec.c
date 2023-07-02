010128d4f58acdc81805cd44bcf0774169b40074
balgxmr <jose@pixelos.net>
2023-07-02 14:18:41 -0500

    Revert "FROMLIST: mm: multi-gen LRU: support page table walks"
    This reverts commit c0367061afe59ff217b96d03a2c19313d92f14ff.

--- fs/exec.c
+++ fs/exec.c
@@ -1052,7 +1052,6 @@ static int exec_mmap(struct mm_struct *mm)
 	activate_mm(active_mm, mm);
 	if (IS_ENABLED(CONFIG_ARCH_WANT_IRQS_OFF_ACTIVATE_MM))
 		local_irq_enable();
-	lru_gen_use_mm(mm);
 	tsk->mm->vmacache_seqnum = 0;
 	vmacache_flush(tsk);
 	lru_gen_add_mm(mm);
