06e0eb1f31a9556c3c13806925c071e00272e10f
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-08-17 06:47:38 +0800

    Revert "msm: kgsl: Use kthread instead of workqueue for event work"
    This reverts commit a15ef12ad8057113fc3fc7b3d6a7bf2387e7a972.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -626,11 +626,11 @@ int kgsl_context_init(struct kgsl_device_private *dev_priv,
 	if (id == -ENOSPC) {
 		/*
 		 * Before declaring that there are no contexts left try
-		 * flushing the event worker just in case there are
+		 * flushing the event workqueue just in case there are
 		 * detached contexts waiting to finish
 		 */
 
-		kthread_flush_worker(device->events_worker);
+		flush_workqueue(device->events_wq);
 		id = _kgsl_get_context_id(device);
 	}
 
@@ -5111,7 +5111,8 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 				PM_QOS_CPU_DMA_LATENCY,
 				PM_QOS_DEFAULT_VALUE);
 
-	device->events_worker = kthread_create_worker(0, "kgsl-events");
+	device->events_wq = alloc_workqueue("kgsl-events",
+		WQ_MEM_RECLAIM | WQ_SYSFS | WQ_HIGHPRI, 0);
 
 	/* Initialize the snapshot engine */
 	kgsl_device_snapshot_init(device);
@@ -5124,9 +5125,6 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 error_close_mmu:
 	kgsl_mmu_close(device);
 error_pwrctrl_close:
-	if (!IS_ERR(device->events_worker))
-		kthread_destroy_worker(device->events_worker);
-
 	kgsl_pwrctrl_close(device);
 error:
 	_unregister_device(device);
@@ -5136,7 +5134,7 @@ EXPORT_SYMBOL(kgsl_device_platform_probe);
 
 void kgsl_device_platform_remove(struct kgsl_device *device)
 {
-	kthread_destroy_worker(device->events_worker);
+	destroy_workqueue(device->events_wq);
 
 	kfree(device->dev->dma_parms);
 	device->dev->dma_parms = NULL;
